b270afa25c3c2119efaad6195633d4bd9c3f5b72
Hyunseung Yun <108418549+MStechYun@users.noreply.github.com>
2024-02-18 15:47:26 +0900

    Title: 달에서 무게 구하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240218/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    cout << fixed;
+    
+    int a = 13;
+    double b = 0.165000;
+    double c = 2.145000;
+
+    cout.precision(6);
+    cout << a << " * " << b << " = " << c;
+
+    return 0;
+}
