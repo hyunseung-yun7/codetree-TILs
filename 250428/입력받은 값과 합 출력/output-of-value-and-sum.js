a65e7bac24222d9bdc66f1d802141b8854abf048
Hyunseung Yun <108418549+hyunseung-yun7@users.noreply.github.com>
2025-04-28 23:01:06 +0900

    Title: 입력받은 값과 합 출력, Time: 164ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250428/입력받은 값과 합 출력/output-of-value-and-sum.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(' ');
+
+let a = parseInt(input[0]);
+let b = parseInt(input[1]);
+
+console.log(a + ' ' + b + ' ' + (a + b));
