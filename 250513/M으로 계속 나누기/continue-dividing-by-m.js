4ebbb7709d806a2dd1f0bd57a400e3ac50037604
Hyunseung Yun <108418549+hyunseung-yun7@users.noreply.github.com>
2025-05-13 19:17:52 +0900

    Title: M으로 계속 나누기, Time: 182ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250513/M으로 계속 나누기/continue-dividing-by-m.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let a = fs.readFileSync(0).toString();
+let arr = a.split(" ");
+
+let n = arr[0];
+let m = arr[1];
+
+// Please write your code here.
+if(n >= 2 && m <= 1000000) {
+    while (n > 0) {
+    console.log(n);
+    n = Math.floor(n /m);
+    }   
+}
