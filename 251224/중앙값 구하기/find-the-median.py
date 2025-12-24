08090bfd162f4c9a9751490514bde38c2708d37c
Hyunseung Yun <108418549+hyunseung-yun7@users.noreply.github.com>
2025-12-24 16:46:32 +0900

    Title: 중앙값 구하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251224/중앙값 구하기/find-the-median.py
@@ -0,0 +1,18 @@
+A, B, C = map(int, input().split())
+
+if A > B:
+    if B > C:
+        print(B)
+    else:
+        if A > C:
+            print(C)
+        else:
+            print(A)
+else:
+    if A > C:
+        print(A)
+    else:
+        if B > C:
+            print(C)
+        else:
+            print(B)
